4138d405a45d89c8ebd9682baceb83dc425b6e72
Ahtisham-Shakir <ahtishamshakir777@gmail.com>
2023-09-29 01:05:18 +0500

    post API

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,1 +1,13 @@
-exports.getFeed = (req, res, next) => {};
+exports.getPosts = (req, res, next) => {
+  res.status(200);
+};
+
+exports.createPost = (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.content;
+
+  res.status(201).json({
+    message: "Post Created Successfully",
+    post: { id: new Date().toISOString(), title: title, content: content },
+  });
+};
