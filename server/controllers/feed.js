d558ef73785dee3b6e101d6c23fbd566dd7f9cde
Ahtisham-Shakir <ahtishamshakir777@gmail.com>
2023-10-03 22:44:56 +0500

    added delete post and pagination

--- server/controllers/feed.js
+++ server/controllers/feed.js
@@ -5,11 +5,23 @@ const { validationResult } = require("express-validator");
 const Post = require("../models/post");
 
 exports.getPosts = (req, res, next) => {
+  const page = +req.query.page || 1;
+  const perPage = 2;
+  let totalItems;
+
   Post.find()
+    .countDocuments()
+    .then((count) => {
+      totalItems = count;
+      return Post.find()
+        .skip((page - 1) * perPage)
+        .limit(perPage);
+    })
     .then((posts) => {
       res.status(200).json({
         message: "Posts fetched successfully",
         posts: posts,
+        totalItems: totalItems,
       });
     })
     .catch((err) => {
@@ -128,6 +140,31 @@ exports.updatePost = (req, res, next) => {
     })
     .then((result) => {
       res.status(200).json({ message: "Post updated", post: result });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+exports.deletePost = (req, res, next) => {
+  const postId = req.params.postId;
+
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const err = new Error("Could not find post");
+        err.statusCode = 422;
+        throw err;
+      }
+      // Check logged in user
+      clearImage(post.imageUrl);
+      return Post.findByIdAndRemove(postId);
+    })
+    .then((result) => {
+      res.status(200).json({ message: "Post Deleted" });
     })
     .catch((err) => {
       if (!err.statusCode) {
